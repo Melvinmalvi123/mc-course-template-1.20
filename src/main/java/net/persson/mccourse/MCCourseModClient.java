7b6ac60506b55c6dc8b1c23f66f899e0401b808a
Persson03 <sagamelvin@hotmail.com>
2025-06-01 04:43:11 +0200

    Initial commit

---
+++ src/main/java/net/persson/mccourse/MCCourseModClient.java
@@ -0,0 +1,13 @@
+package net.persson.mccourse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MCCourseModClient implements ClientModInitializer
+{
+
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
